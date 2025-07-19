4aae933e67831b5909100958692051d3518f1806
GaelAlburo <gael.a24@outlook.com>
2025-07-19 17:33:35 -0600

    feat: add security layer with Bearer token authentication

---
+++ src/main/java/com/gaelalburo/hackingmx25/autoahorroapi/configuration/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.gaelalburo.hackingmx25.autoahorroapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Auto Ahorro API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
+}
