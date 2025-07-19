c5e8177f7b2c025ac5732dafd0724040825aa321
GaelAlburo <gael.a24@outlook.com>
2025-07-19 17:28:25 -0600

    chore: initialize Spring Boot project

--- src/main/java/com/gaelalburo/hackingmx25/autoahorroapi/AutoahorroApiApplication.java
+++ src/main/java/com/gaelalburo/hackingmx25/autoahorroapi/AutoahorroApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoahorroApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoahorroApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoahorroApiApplication.class, args);
+    }
 
 }
