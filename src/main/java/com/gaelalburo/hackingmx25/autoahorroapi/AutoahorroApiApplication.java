e4b0234a88d82a3789532fae98d398597ef51f41
GaelAlburo <gael.a24@outlook.com>
2025-07-19 17:24:55 -0600

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/gaelalburo/hackingmx25/autoahorroapi/AutoahorroApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaelalburo.hackingmx25.autoahorroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoahorroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoahorroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaelalburo/hackingmx25/autoahorroapi/AutoahorroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaelalburo.hackingmx25.autoahorroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoahorroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
